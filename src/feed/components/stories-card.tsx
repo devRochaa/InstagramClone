78788a4781f332c4a003534ce424bf8b664b3a0c
Daniel <danielrocha.etec@gmail.com>
2025-09-28 22:20:52 -0300

    fix: storie-row

--- src/feed/components/stories-card.tsx
+++ src/feed/components/stories-card.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, {forwardRef } from "react";
 
 type Props = {
   photo: string;
   username: string;
 };
 
-const StoriesCard = ({ photo, username }: Props) => {
+const StoriesCard =  forwardRef<HTMLDivElement, Props>(({ photo, username } : Props, ref) => {
   return (
     <>
-      <div className="flex flex-col shrink-0 items-center justify-center cursor-pointer ">
+      <div className="flex flex-col shrink-0 items-center justify-center cursor-pointer " ref={ref}>
         <div className="p-[3px] rounded-full bg-gradient-to-tr from-yellow-500 via-pink-500 to-purple-600">
           <div className="rounded-full p-[2px] bg-black">
             <img className="rounded-full w-19" src={photo} alt="Icone" />
@@ -18,6 +18,6 @@ const StoriesCard = ({ photo, username }: Props) => {
       </div>
     </>
   );
-};
+});
 
 export default StoriesCard;
--- src/feed/components/stories-row.tsx
+++ src/feed/components/stories-row.tsx
@@ -1,49 +1,117 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import StoriesCard from "./stories-card";
 import mugshotPic from "../../img/istockphoto-672074756-mugshot.jpg";
 import gatoazulPic from "../../img/gatoazul.jpg";
 import gatopic from "../../img/foto.jpeg";
 
+const storiesData = [
+  { photo: mugshotPic, username: "daniel" },
+  { photo: gatopic, username: "rocha" },
+  { photo: mugshotPic, username: "teste1" },
+  { photo: gatopic, username: "teste2" },
+  { photo: mugshotPic, username: "teste3" },
+  { photo: gatopic, username: "teste4" },
+  { photo: mugshotPic, username: "daniel" },
+  { photo: gatopic, username: "rocha" },
+  { photo: mugshotPic, username: "teste1" },
+  { photo: gatopic, username: "teste2" },
+  { photo: mugshotPic, username: "teste3" },
+  { photo: gatopic, username: "teste4" },
+  { photo: mugshotPic, username: "daniel" },
+  { photo: gatopic, username: "rocha" },
+  { photo: mugshotPic, username: "teste1" },
+  { photo: gatopic, username: "teste2" },
+  { photo: mugshotPic, username: "teste3" },
+  { photo: gatopic, username: "teste4" },
+  { photo: gatopic, username: "teste4" },
+  { photo: gatopic, username: "teste4" },
+  { photo: gatopic, username: "teste4" },
+  { photo: gatopic, username: "teste4" },
+];
+
 const StoriesRow = () => {
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(true);
+  const [cardWidth, setCardWidth] = useState(0);
+  
   const scrollRef = useRef<HTMLDivElement>(null);
+  const cardRef = useRef<HTMLDivElement>(null);
+  
+  // mede a largura de 1 card assim que monta
+  useEffect(() => {
+    if (cardRef.current) {
+      setCardWidth(cardRef.current.offsetWidth + 20); // +20px do gap-5
+    }
+    checkScrollPosition();
+  }, []);
+
+  const checkScrollPosition = () => {
+    if (!scrollRef.current) return;
+
+    const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current;
+
+    const step = cardWidth * 6;
+    const remaining = scrollWidth - (scrollLeft + clientWidth);
+    const move = remaining < step ? remaining : step;
+
+    setCanScrollLeft(scrollLeft > 1);
+    setCanScrollRight((scrollLeft + clientWidth - scrollWidth < remaining)); 
+    console.log(scrollLeft, clientWidth, scrollLeft + clientWidth, scrollWidth);
+    
+    // -1 só pra evitar bug de arredondamento
+  };
 
   const scrollLeft = () => {
-    scrollRef.current?.scrollBy({ left: -734, behavior: "smooth" });
+    if (!scrollRef.current) return;
+    const step = cardWidth * 6;
+
+    scrollRef.current?.scrollBy({ left: -step, behavior: "smooth" });
+    setTimeout(checkScrollPosition, 300); // espera a animação 
+
   };
   const scrollRight = () => {
-    scrollRef.current?.scrollBy({ left: 734, behavior: "smooth" });
-  };
+    if (!scrollRef.current) return;
 
-  const tamanho = Array(20).fill(null);
+    const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current;
+    const step = cardWidth * 6;
+    // quanto falta até o fim
+    const remaining = scrollWidth - (scrollLeft + clientWidth);
+
+    const move = remaining < step ? remaining : step;
+
+    scrollRef.current?.scrollBy({ left: move, behavior: "smooth" });
+    setTimeout(checkScrollPosition, 300);   
+  };
 
   return (
     <div className="flex w-full overflow-hidden pl-10 items-center justify-center gap-3 min-h-[170px]">
       {/* <div className="relative w-full min-h-max overflow-hidden border-b border-zinc-800 py-6 px-15"> */}
-      <div className="relative mb-5">
-        <button
+      <div className="relative flex items-center">
+        {canScrollLeft && <button
           // className="absolute w-6 h-6 left-12 top-1/2 -translate-y-1/2 z-10 bg-white rounded-full shadow-2xl cursor-pointer font-sans"
           className="w-[24px] h-[24px] absolute -translate-y-1/2 z-10 bg-white rounded-full shadow-2xl cursor-pointer font-sans"
           onClick={scrollLeft}
         >
           {"<"}
-        </button>
+        </button>}
+        
       </div>
 
       <div
         ref={scrollRef}
         className="flex flex-row gap-5 overflow-x-scroll scrollbar-hide w-155 mb-3 mr-9"
       >
-        {tamanho.map(() => {
-          return (
-            <>
-              <StoriesCard photo={gatoazulPic} username="daniel" />
-              <StoriesCard photo={mugshotPic} username="rocha" />
-              <StoriesCard photo={gatopic} username="teste" />
-            </>
-          );
+        {storiesData.map((data, i) => {
+            if(i == 0){
+              console.log("foi");
+              
+             return <StoriesCard key={i} ref={cardRef} photo={data.photo} username={data.username} />;
+            }
+            return <StoriesCard key={i} photo={data.photo} username={data.username} />
         })}
       </div>
-      <div className="relative mb-5">
+      <div className="relative flex items-center">
+        {canScrollRight && (
         <button
           className="w-[24px] h-[24px] absolute right-10 -translate-y-1/2 z-11 bg-white rounded-full shadow-2xl cursor-pointer"
           // className="absolute right-15 top-1/2 w-6 h-6 -translate-y-1/2 z-11 bg-white rounded-full shadow-2xl cursor-pointer"
@@ -51,6 +119,8 @@ const StoriesRow = () => {
         >
           {">"}
         </button>
+
+        )}
       </div>
     </div>
   );
